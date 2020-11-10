5985b3ca71b8f85e7d099cc3de17ae3d7be1ee98
oliver <oliver.kali.hub@gmail.com>
2020-11-10 12:59:52 -0600

    Listo

---
+++ index.js
@@ -0,0 +1,87 @@
+$(document).ready(() => {
+    const img = [
+        {
+            urlImg: './img/card/913381.jpg',
+            title: 'Capitulo'
+        },
+        {
+            urlImg: './img/card/1050011.jpg',
+            title: 'Capitulo'
+        },
+        {
+            urlImg: './img/card/cropped-1366-768-1073623.jpg',
+            title: 'Capitulo'
+        },
+        {
+            urlImg: './img/card/cropped-1366-768-913380.jpg',
+            title: 'Capitulo'
+        },
+        {
+            urlImg: './img/card/1050011.jpg',
+            title: 'Capitulo'
+        },
+        {
+            urlImg: './img/card/cropped-1366-768-1073623.jpg',
+            title: 'Capitulo'
+        },
+        {
+            urlImg: './img/card/913381.jpg',
+            title: 'Capitulo'
+        },
+        {
+            urlImg: './img/card/1050011.jpg',
+            title: 'Capitulo'
+        },
+        {
+            urlImg: './img/card/cropped-1366-768-1073623.jpg',
+            title: 'Capitulo'
+        },
+        {
+            urlImg: './img/card/cropped-1366-768-913380.jpg',
+            title: 'Capitulo'
+        },
+    ]
+
+    const sectionCard = $('.section-card')
+
+    $(window).on('load' , () => createCar(img , sectionCard))
+
+    $(window).on('scroll' , () => scrollNav())
+})
+
+const scrollNav = () => {
+    var scroll = $(window).scrollTop()
+
+    scroll < 100 
+        ? $('.header-section').removeClass('activ')
+        : $('.header-section').addClass('activ')
+}
+
+const createCar = (imgData , sectionCard) => {
+    $.each(imgData , (key , value) => {
+        sectionCard.append(
+            `
+                <div class="card-serie">
+                    <img src=${value.urlImg} alt="">
+                    <h2 class="card-serie__title">
+                        ${value.title + key} 
+                    </h2>
+                </div>
+            `
+        )
+    })
+
+    const title = $('.card-serie__title')
+
+    
+
+    $('.card-serie').each((key , value) => {
+        $(value).hover(() => {
+            $(value).addClass('activ')
+        }, () => {
+            $(value).removeClass('activ')
+        })
+    })
+
+    
+}
